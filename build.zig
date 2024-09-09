e44a4dd582265181a24c9d0185f5896c68606f57
Darkfllame <ntmconar5@gmail.com>
2024-09-09 16:28:09 +0200

    changed from src/lib.zig to src/zgll.zig

--- build.zig
+++ build.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("zgll", .{
-        .root_source_file = b.path("src/lib.zig"),
+        .root_source_file = b.path("src/zgll.zig"),
     });
 }
